524e3128dab6809e5590e7fec120a02c2c5edb76
Mario Arana <mariorobertoarana@gmail.com>
2025-12-16 00:08:38 -0300

    corrección

---
+++ Practicas_de_C/Guia_7_Matrices/Ejercicio_6.c
@@ -0,0 +1,3 @@
+/*Crea un programa que multiplique dos matrices y muestre el resultado. Aseg´urate
+de que las dimensiones de las matrices sean compatibles para la multiplicaci´on.
+Muestra tambi´en las matrices originales y el resultado de la multiplicaci´on.*/
